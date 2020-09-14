d2c2e29373b5d495be48855e64f0b1c5211dd77e
Spencer Paciello <spencerpaciello3@gmail.com>
2020-09-13 23:29:19 -0400

    changes name

--- Package.swift
+++ Package.swift
@@ -4,14 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "alerts-and-pickers-new 3",
+    name: "alerts-and-pickers",
     platforms: [
-        .iOS(.v12),
-    ], products: [
+            .iOS(.v12),
+    ],
+    products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "alerts-and-pickers-new 3",
-            targets: ["alerts-and-pickers-new 3"]),
+            name: "alerts-and-pickers",
+            targets: ["alerts-and-pickers"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -21,10 +22,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "alerts-and-pickers-new 3",
+            name: "alerts-and-pickers",
             dependencies: []),
         .testTarget(
-            name: "alerts-and-pickers-new 3Tests",
-            dependencies: ["alerts-and-pickers-new 3"]),
+            name: "alerts-and-pickers Tests",
+            dependencies: ["alerts-and-pickers"]),
     ]
 )
